033c4c4ee9637fa4b32c9c18d9767196976e8e63
Saif Alhaider <saifharith90@gmail.com>
2022-12-16 20:25:02 +0300

    adding triangle and changing colors

--- src/card/card-content.js
+++ src/card/card-content.js
@@ -1,25 +1,42 @@
 function Card({ first_second_columns }) {
+  const triggerColor = "white";
   return (
+    // the parent
     <div
-      className="grid grid-cols-3 w-full h-full  "
+      className="grid grid-cols-3 w-full h-full"
       style={{ direction: "rtl" }}
     >
+      {/* first column on the right */}
       <div
-        className="bg-slate-700 overflow-scroll h-auto overflow-x-hidden col-start-1 col-end-2 text-right scrollbar-thin scrollbar-thumb-rose-500  scrollbar-track-slate-700 scroll-smooth"
-        style={{ direction: "ltr" }}
+        className="bg-[#F0F3F6] h-auto"
+        style={{
+          direction: "ltr",
+        }}
       >
-        <ul className="list-none relative p-4 overflow-hidden">
+        <ul className="list-none relative p-[2px] h-96">
           <div className="relative">
-            <div className="absolute w-full bg-slate-50 rounded-2xl h-16"></div>
+            {/* when hovering on mouse it moves */}
+            <div
+              className={`absolute w-full bg-${triggerColor} rounded-md h-16`}
+            >
+              {/* the triangle */}
+              <div className={`border-b-[20px] border-b-${triggerColor} border-r-[20px] border-r-transparent  rotate-45 absolute left-[-8px] top-[50%] translate-y-[-50%]`}
+              style={{borderBottomLeftRadius:"4px"}}
+              ></div>
+            </div>
           </div>
-          {Object.keys(first_second_columns).map((e,i) => (
-            <li className="relative text-center h-16 align-middle flex items-center justify-center">
-            {e}
-          </li>
+
+          {Object.keys(first_second_columns).map((e, i) => (
+            <div>
+              <li className="relative text-center h-16 align-middle flex items-center justify-center">
+                {e}
+              </li>
+            </div>
           ))}
         </ul>
       </div>
-      <div className="bg-green-300 col-start-2 col-end-4">2</div>
+      {/* second column */}
+      <div className="bg-[#F0F3F6] col-start-2 col-end-4 relative -z-10"></div>
     </div>
   );
 }
